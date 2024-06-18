8b4c4838b911f3fdf6286aff182466b86615d5d7
Stark-Industries7 <apooravmukherjee@gmail.com>
2024-06-18 20:02:16 +0530

    All changes made and errors resolved

--- main.py
+++ main.py
@@ -1,43 +1,54 @@
-theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ', 
-            'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ', 
-            'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
+theBoard = {
+    "top-L": " ",
+    "top-M": " ",
+    "top-R": " ",
+    "mid-L": " ",
+    "mid-M": " ",
+    "mid-R": " ",
+    "low-L": " ",
+    "low-M": " ",
+    "low-R": " ",
+}
+
 
 def printBoard(board):
-    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
-    print('-+-+-')
-    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
-    print('-+-+-')
-    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+    print(board["top-L"] + "|" + board["top-M"] + "|" + board["top-R"])
+    print("-+-+-")
+    print(board["mid-L"] + "|" + board["mid-M"] + "|" + board["mid-R"])
+    print("-+-+-")
+    print(board["low-L"] + "|" + board["low-M"] + "|" + board["low-R"])
 
-def winGame(board,turn):
+
+def winGame(board, turn):
 
     # Check rows
-    for row in ['top', 'mid', 'low']:
-        if board[row + '-L'] == board[row + '-M'] == board[row + '-R'] != ' ':
+    for row in ["top", "mid", "low"]:
+        if board[row + "-L"] == board[row + "-M"] == board[row + "-R"] != " ":
             print(turn + " won!")
             return True
 
     # Check columns
-    for col in ['L', 'M', 'R']:
-        if board['top-' + col] == board['mid-' + col] == board['low-' + col] != ' ':
+    for col in ["L", "M", "R"]:
+        if board["top-" + col] == board["mid-" + col] == board["low-" + col] != " ":
             print(turn + " won!")
             return True
 
     # Check diagonals
-    if board['top-L'] == board['mid-M'] == board['low-R'] != ' ':
+    if board["top-L"] == board["mid-M"] == board["low-R"] != " ":
         print(turn + " won!")
         return True
-    if board['top-R'] == board['mid-M'] == board['low-L'] != ' ':
+    if board["top-R"] == board["mid-M"] == board["low-L"] != " ":
         print(turn + " won!")
         return True
 
     return False
 
+
 def game(board):
     turn = "X"
     for i in range(9):
         printBoard(board)
-        print("Turn For ",turn,".Move On Which Space?")
+        print("Turn For ", turn, ".Move On Which Space?")
         move = input()
         board[move] = turn
         if winGame(board, turn):
@@ -48,8 +59,8 @@ def game(board):
         else:
             turn = "X"
     printBoard(board)
-    print("It's a tie!")     
+    print("It's a tie!")
+
 
 if __name__ == "__main__":
-    game(theBoard)       
-
+    game(theBoard)
